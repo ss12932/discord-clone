b1ae3f62ce0c391a08fc70274b2cd456cef1bf59
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-31 12:57:09 +0100

    added serverImage upload functionality + edit

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,4 +1,9 @@
 "use client";
+import { X } from "lucide-react";
+import Image from "next/image";
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
@@ -7,5 +12,31 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  return <div>File Upload</div>;
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(err: Error) => {
+        console.log(err);
+      }}
+    />
+  );
 };
