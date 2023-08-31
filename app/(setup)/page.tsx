3fc74e16542f21d89b6787d11e6dc2dfa99b6f62
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-31 12:38:55 +0100

    uploadthing setup + file upload component

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,6 @@
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
-import { initialProfile } from "@/lib/iniital-profile";
+import { initialProfile } from "@/lib/initial-profile";
 import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
