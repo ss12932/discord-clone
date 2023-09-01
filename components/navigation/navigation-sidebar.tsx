84abd2ee664b19c624aa61bce32702300018b00b
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-01 12:14:37 +0100

    added icon tooltip

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,6 +1,9 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { NavigationAction } from "@/components/navigation/navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -21,7 +24,13 @@ const NavigationSidebar = async () => {
 
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-      Navigation Side bar
+      <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id}>{server.name}</div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
