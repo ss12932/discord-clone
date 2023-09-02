defad5a0fc95503b7cc7c8b899f115cca1840495
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-02 12:50:37 +0100

    added server sidebar + theming

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,7 +1,8 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
-import { Divide } from "lucide-react";
+import { Divide, Server } from "lucide-react";
 import { redirect } from "next/navigation";
 
 const ServerIdLayout = async ({
@@ -33,7 +34,14 @@ const ServerIdLayout = async ({
     return redirect("/");
   }
 
-  return <div>{children}</div>;
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col  fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
 };
 
 export default ServerIdLayout;
