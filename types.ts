80c63759d9bcfad6d8e9117e1943f9912155ecf9
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-04 11:15:59 +0100

    fix type error

--- types.ts
+++ types.ts
@@ -1,6 +1,5 @@
 import { Member, Profile, Server } from "@prisma/client";
 
-export type ServerWithMembersWithProfiles = Server &
-  {
-    members: Member & { profile: Profile };
-  }[];
+export type ServerWithMembersWithProfiles = Server & {
+  members: (Member & { profile: Profile })[];
+};
