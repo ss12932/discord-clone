d2d869d33f9dfc3a5f439b2d2642696978a2f627
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-05 21:40:47 +0100

    delete server fn added + search search added

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ServerHeader } from "./server-header";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ServerSearch } from "@/components/server/server-search";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -60,6 +62,11 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch />
+        </div>
+      </ScrollArea>
     </div>
   );
 };
