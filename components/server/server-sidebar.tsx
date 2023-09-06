39be83af95df567fe47e21ff70324b02835c623f
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-06 12:59:45 +0100

    add server section to server sidebar

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -6,6 +6,8 @@ import { ServerHeader } from "./server-header";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { ServerSearch } from "@/components/server/server-search";
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { ServerSection } from "@/components/server/server-section";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -120,6 +122,12 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             ]}
           />
         </div>
+        <Separator className="bg-zinc-500 dark:bg-zinc-700 rounded-md my-2" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection />
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
