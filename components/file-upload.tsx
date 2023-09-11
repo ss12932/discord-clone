27298c45094bd51724646effebaed66278a93174
ss12932 <ss12932@my.bristol.ac.uk>
2023-09-11 14:55:29 +0100

    finish file upload config

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 
@@ -29,18 +29,38 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
     );
   }
 
-  if (value && fileType === "pdf" {
-    
-  })
+  if (value && fileType === "pdf") {
     return (
-      <UploadDropzone
-        endpoint={endpoint}
-        onClientUploadComplete={(res) => {
-          onChange(res?.[0].url);
-        }}
-        onUploadError={(err: Error) => {
-          console.log(err);
-        }}
-      />
+      <div className="relative mt-2 flex items-center rounded-md bg-background/10 p-2">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 hover:underline dark:text-indigo-400"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="absolute -right-2 -top-2 rounded-full bg-rose-500 p-1 text-white shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
     );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(err: Error) => {
+        console.log(err);
+      }}
+    />
+  );
 };
